7b793f1d6d390534a500e7717fdac863d2bda2e0
Ignacio Barcelo <ignacio.barcelo@poliglota.org>
2023-08-18 09:04:49 -0400

    create go workspace

---
+++ 2/data-types/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	var title string
+	var copies int
+	var edition int
+	title = "For the Love of Go"
+	copies = 99
+	edition = 4
+	fmt.Println(title)
+	fmt.Println(copies, edition)
+}
