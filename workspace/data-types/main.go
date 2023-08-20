dad3256b7d0dcd9a1982fd1ad967fc54ad8df277
Ignacio Barcelo <ignacio.barcelo@poliglota.org>
2023-08-20 17:34:21 -0400

    modify workspace, create first test in bookstore

--- workspace/data-types/main.go
+++ workspace/data-types/main.go
@@ -6,9 +6,13 @@ func main() {
 	var title string
 	var copies int
 	var edition int
+	var specialOffer bool
+	var discountPercentage float64
 	title = "For the Love of Go"
 	copies = 99
 	edition = 4
+	specialOffer = true
+	discountPercentage = 10
 	fmt.Println(title)
-	fmt.Println(copies, edition)
+	fmt.Println(copies, edition, specialOffer, discountPercentage)
 }
